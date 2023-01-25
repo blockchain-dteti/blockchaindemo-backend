4cf4bbe69410ce189513e9a0e69eb44be03bebfc
karuniaperjuangan <karuniaperjuangan@mail.ugm.ac.id>
2023-01-25 19:51:53 +0700

    update

--- server.ts
+++ server.ts
@@ -3,8 +3,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const app: Express = express();
-const port = process.env.PORT;
+const app = express();
+const port = process.env.PORT || 5500;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
